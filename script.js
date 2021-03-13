acf9b9028c82269e8b112b1841ac4465435f6388
escreva o seu nome <jvbarabba@gmail.com>
2021-03-13 01:08:44 -0300

    :package: Finalizamos o nosso Sidebar e aplicamos nele JavaScript

---
+++ script.js
@@ -0,0 +1,3 @@
+document.querySelector(".hamburguer").addEventListener("click", () =>
+    document.querySelector(".container").classList.toggle("show-menu")
+); //Neste JS declaro a variavel Hamburguer e adciono um evento de click nela,assim quando o click ocorre a classe show menu acontece
