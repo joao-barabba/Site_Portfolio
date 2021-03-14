1743188c9a1bb1ff89ade866561e7e02a390f108
escreva o seu nome <jvbarabba@gmail.com>
2021-03-14 11:48:23 -0300

    :package: Hoje entrego a sessão de Conhecimentos

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Site_Portfolio
+
 Neste novo projeto darei inicio a um site onde usarei como meu portfólio próprio, nele disponibilizarei meus dados de contato e projetos em qual já trabalhei ou estou trabalhando, usarei conhecimentos nele de HTML, CSS e JS
 
 ## Tarefas
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-document.querySelector(".hamburguer").addEventListener("click", () =>
+document
+  .querySelector(".hamburguer")
+  .addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
-); //Neste JS declaro a variavel Hamburguer e adciono um evento de click nela,assim quando o click ocorre a classe show menu acontece
+  ); //Neste JS declaro a variavel Hamburguer e adciono um evento de click nela,assim quando o click ocorre a classe show menu acontece
